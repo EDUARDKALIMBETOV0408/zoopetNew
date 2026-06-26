54350630d56988e3a01797c62c18a1d701c414f3
EDUARDKALIMBETOV0408 <kalimbetov.eduard@gmail.com>
2026-06-27 00:10:22 +0300

    Create index.js

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+// src/store/index.js
+export function createStore(initialState, reducer) {
+    let state = initialState;
+    const listeners = [];
+
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener(state));
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            const index = listeners.indexOf(listener);
+            if (index !== -1) listeners.splice(index, 1);
+        };
+    };
+
+    return { getState, dispatch, subscribe };
+}
